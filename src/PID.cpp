f9f74b463349a77f5199ceb15af22be44a1719bc
bogdanvbalan <bogdan.v.balan@gmail.com>
2020-01-16 12:10:44 +0000

    Add d controller

--- src/PID.cpp
+++ src/PID.cpp
@@ -17,12 +17,18 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   Ki = Ki_;
   Kd = Kd_;
   
+  p_error = 0.0;
+  d_error = 0.0;
+  prev_cte = 0.0;
+  
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
+  
+  d_error = cte - p_error;
   p_error = cte;
   
 }
@@ -31,5 +37,5 @@ double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return -Kp * p_error;  // TODO: Add your total error calc here!
+  return -Kp * p_error - Kd * d_error;  // TODO: Add your total error calc here!
 }
